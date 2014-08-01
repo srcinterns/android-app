3223a581efabed6f8f1be3d7ecf6dd0f025761b8
Eric Richter <richter.eric.r@gmail.com>
2014-08-01 12:31:24 -0400

    Refactor MainActivity
    Used some sample code from http://pastebin.com/J4uDgrZ8, retrofitted to our purposes

--- Candar/src/src/interns/candar/MainActivity.java
+++ Candar/src/src/interns/candar/MainActivity.java
@@ -1,90 +1,100 @@
 package src.interns.candar;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.graphics.Canvas;
-import android.graphics.Paint;
+import android.graphics.SurfaceTexture;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.Gravity;
 import android.view.TextureView;
+import android.widget.FrameLayout;
 
-public class MainActivity extends ActionBarActivity implements Listener {
-	
-	ArrayList<Integer> colorArray = new ArrayList<Integer>();
-	UDPlistener listener = new UDPlistener(this);
-	int offset = 0;
-	TextureView tv;
+@SuppressWarnings({"UnusedDeclaration"})
+public class MainActivity extends Activity implements TextureView.SurfaceTextureListener {
+    private TextureView mTextureView;
+    private MainActivity.RenderingThread mThread;
+    private UDPlistener mUdp;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		listener.start();
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		tv = (TextureView)findViewById(R.id.textureView1);
-	}
-	
-	
-	@Override
-	public void onDestroy(){
-		super.onDestroy();
-		listener.end();
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+        FrameLayout content = new FrameLayout(this);
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+        mTextureView = new TextureView(this);
+        mTextureView.setSurfaceTextureListener(this);
+        mTextureView.setOpaque(false);
 
-	@Override
-	public void update(int[] array) {
-		for(int i = 0; i < 512; i++)
-		{
-			colorArray.add(array[i]);
-		}
-		int[] colors = new int[colorArray.size()];
-		colors = convertIntegers(colorArray);
-		//updateOffset(colors);
-		Canvas canvas = new Canvas();
-		Paint paint = new Paint();
-		canvas.drawBitmap(colors, offset, 512, 0, 0, 512, 1, false, paint);
-		tv.draw(canvas);
-	}
-	
-	public static int[] convertIntegers(List<Integer> integers)
-	{
-	    int[] ret = new int[integers.size()];
-	    Iterator<Integer> iterator = integers.iterator();
-	    for (int i = 0; i < ret.length; i++)
-	    {
-	        ret[i] = iterator.next().intValue();
-	    }
-	    return ret;
-	}
-	
-	public void updateOffset(int[] array)
-	{
-		if(array.length >= 2048)
-		{
-			offset = 512; //Not correct
-		}
-	}
+        content.addView(mTextureView, new FrameLayout.LayoutParams(500, 500, Gravity.CENTER));
+        setContentView(content);
+    }
+
+    @Override
+    public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+        mThread = new RenderingThread(mTextureView);
+        mUdp = new UDPlistener(mThread);
+        mThread.start();
+        mUdp.start();
+    }
+
+    @Override
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+        // Ignored
+    }
+
+    @Override
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+        if (mThread != null) mThread.stopRendering();
+        mUdp.end();
+        return true;
+    }
+
+    @Override
+    public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+        // Ignored
+    }
+
+    private static class RenderingThread extends Thread implements Listener {
+        private final TextureView mSurface;
+        private volatile boolean mRunning = true;
+        private int[] colors;
+
+        public RenderingThread(TextureView surface) {
+            mSurface = surface;
+        }
+
+        @Override
+        public void update(int[] colorArray) {
+            colors = colorArray;
+        }
+
+        @Override
+        public void run() {
+
+            while (mRunning && !Thread.interrupted()) {
+                final Canvas canvas = mSurface.lockCanvas(null);
+                try {
+                    if (null == colors) continue;
+                    canvas.drawBitmap(colors, 0, 512, 0, 0, 512, 1, false, null);
+                } catch (Exception e) {
+                    Log.e("RenderThread", "Exception: " + e);
+                } finally {
+                    mSurface.unlockCanvasAndPost(canvas);
+                }
+
+                try {
+                    Thread.sleep(15);
+                } catch (InterruptedException e) {
+                    // Interrupted
+                }
+            }
+        }
+
+        void stopRendering() {
+            interrupt();
+            mRunning = false;
+        }
+
+
+    }
 }
