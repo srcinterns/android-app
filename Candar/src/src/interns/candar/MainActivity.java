e992ae6bb6dd87ba7c62ffd0ad0cae8b294e9335
Emily Kinel <kinelec@clarkson.edu>
2014-08-01 13:26:52 -0400

    Update MainActivity.java

--- Candar/src/src/interns/candar/MainActivity.java
+++ Candar/src/src/interns/candar/MainActivity.java
@@ -1,12 +1,9 @@
 package src.interns.candar;
 
-<<<<<<< HEAD
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-
-=======
->>>>>>> origin/master
 import android.app.Activity;
 import android.graphics.Canvas;
 import android.graphics.SurfaceTexture;
@@ -15,7 +12,7 @@ import android.util.Log;
 import android.view.Gravity;
 import android.view.TextureView;
 import android.widget.FrameLayout;
-<<<<<<< HEAD
+
 
 @SuppressWarnings({"UnusedDeclaration"})
 public class MainActivity extends Activity implements TextureView.SurfaceTextureListener {
@@ -127,97 +124,6 @@ public class MainActivity extends Activity implements TextureView.SurfaceTexture
             mRunning = false;
         }
 
-
-=======
-
-@SuppressWarnings({"UnusedDeclaration"})
-public class MainActivity extends Activity implements TextureView.SurfaceTextureListener {
-    private TextureView mTextureView;
-    private MainActivity.RenderingThread mThread;
-    private UDPlistener mUdp;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        FrameLayout content = new FrameLayout(this);
-
-        mTextureView = new TextureView(this);
-        mTextureView.setSurfaceTextureListener(this);
-        mTextureView.setOpaque(false);
-
-        content.addView(mTextureView, new FrameLayout.LayoutParams(500, 500, Gravity.CENTER));
-        setContentView(content);
-    }
-
-    @Override
-    public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
-        mThread = new RenderingThread(mTextureView);
-        mUdp = new UDPlistener(mThread);
-        mThread.start();
-        mUdp.start();
-    }
-
-    @Override
-    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
-        // Ignored
-    }
-
-    @Override
-    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
-        if (mThread != null) mThread.stopRendering();
-        mUdp.end();
-        return true;
-    }
-
-    @Override
-    public void onSurfaceTextureUpdated(SurfaceTexture surface) {
-        // Ignored
-    }
-
-    private static class RenderingThread extends Thread implements Listener {
-        private final TextureView mSurface;
-        private volatile boolean mRunning = true;
-        private int[] colors;
-
-        public RenderingThread(TextureView surface) {
-            mSurface = surface;
-        }
-
-        @Override
-        public void update(int[] colorArray) {
-            colors = colorArray;
-        }
-
-        @Override
-        public void run() {
-
-            while (mRunning && !Thread.interrupted()) {
-                final Canvas canvas = mSurface.lockCanvas(null);
-                try {
-                    if (null == colors) continue;
-                    canvas.drawBitmap(colors, 0, 512, 0, 0, 512, 1, false, null);
-                } catch (Exception e) {
-                    Log.e("RenderThread", "Exception: " + e);
-                } finally {
-                    mSurface.unlockCanvasAndPost(canvas);
-                }
-
-                try {
-                    Thread.sleep(15);
-                } catch (InterruptedException e) {
-                    // Interrupted
-                }
-            }
-        }
-
-        void stopRendering() {
-            interrupt();
-            mRunning = false;
-        }
-
-
->>>>>>> origin/master
     }
 }
 
