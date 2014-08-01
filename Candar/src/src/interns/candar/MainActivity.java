d3b091a10eb29f51267cff097a69404aabd28608
Eric Richter <richteer@clarkson.edu>
2014-08-01 14:22:31 -0400

    Resolved merge in MainActivity

--- Candar/src/src/interns/candar/MainActivity.java
+++ Candar/src/src/interns/candar/MainActivity.java
@@ -6,20 +6,12 @@ import java.util.Iterator;
 import java.util.List;
 import android.app.Activity;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.SurfaceTexture;
 import android.os.Bundle;
-import android.provider.CalendarContract;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.TextureView;
 import android.widget.FrameLayout;
-
-<<<<<<< HEAD
-import java.util.Arrays;
-
-=======
->>>>>>> e992ae6bb6dd87ba7c62ffd0ad0cae8b294e9335
 
 @SuppressWarnings({"UnusedDeclaration"})
 public class MainActivity extends Activity implements TextureView.SurfaceTextureListener {
@@ -69,30 +61,16 @@ public class MainActivity extends Activity implements TextureView.SurfaceTexture
     private static class RenderingThread extends Thread implements Listener {
         private final TextureView mSurface;
         private volatile boolean mRunning = true;
-<<<<<<< HEAD
         private RotatingArray data = new RotatingArray(512, 800, 10); // TODO: Replace these with width/height constants
-=======
-        private int[] colors;
-        private List<Integer> colorArray = new ArrayList<Integer>();
->>>>>>> e992ae6bb6dd87ba7c62ffd0ad0cae8b294e9335
+
 
         public RenderingThread(TextureView surface) {
             mSurface = surface;
         }
 
         @Override
-<<<<<<< HEAD
         public void update(int[] colorArray) {
             data.append(colorArray);
-=======
-        public void update(int[] array) {
-        	for(int i = 0; i < 512; i++)
-    		{
-    			colorArray.add(array[i]);
-    		}
-    		colors = convertIntegers(colorArray);
-    		Log.d("Color", "" + colors[0]);
->>>>>>> e992ae6bb6dd87ba7c62ffd0ad0cae8b294e9335
         }
         
         public static int[] convertIntegers(List<Integer> integers)
@@ -118,14 +96,7 @@ public class MainActivity extends Activity implements TextureView.SurfaceTexture
             while (mRunning && !Thread.interrupted()) {
                 final Canvas canvas = mSurface.lockCanvas(null);
                 try {
-<<<<<<< HEAD
-                    int[] moo = data.getArray();
-                    canvas.drawBitmap(moo, data.getOffset(), 512, 0, 0, 512, 800, false, null);
-=======
-                    if (null == colors) continue;
-                    canvas.drawBitmap(colors, 0, 512, 0, 0, 512, 300, false, null);
-        
->>>>>>> e992ae6bb6dd87ba7c62ffd0ad0cae8b294e9335
+                    canvas.drawBitmap(data.getArray(), data.getOffset(), 512, 0, 0, 512, 800, false, null);
                 } catch (Exception e) {
                     Log.e("RenderThread", "Exception: " + e);
                 } finally {
